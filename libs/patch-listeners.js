2206211c6986882738e38c42bc5c718c1f4b8c90
César <cesarga@d3riva.com>
2021-03-04 12:53:56 +0100

    Update patch-listeners.js

--- libs/patch-listeners.js
+++ libs/patch-listeners.js
@@ -1,22 +1,40 @@
-function patchListeners() {
+function patchListeners(options = {}) {
+  options = Object.assign({}, {
+    type: null,
+    registerCallback: null,
+    eventCallback: null,
+  }, options);
   window.listeners = [];
   const _addEventListener = EventTarget.prototype.addEventListener;
   EventTarget.prototype.addEventListener = function(...args) {
     if (this instanceof HTMLElement) {
+      if ((null === options.type) || options.type === args[0]) {
+        if (null !== options.registerCallback) {
+          options.registerCallback({
+            target: this,
+            args: args,
+          });
+        }
+
         const _callback = args[1];
         args[1] = function(...args) {
           if (window.DEBUG_LISTENERS) {
             console.log(args);
           }
+
+          if (null !== options.eventCallback) {
+            options.eventCallback(...args);
+          }
+
           return _callback.apply(this, args);
         }
-      
-      window.listeners.push({
-        type: args[0],
-        fn: args[1],
-        target: this,
-      });
-      
+
+        window.listeners.push({
+          type: args[0],
+          fn: args[1],
+          target: this,
+        });
+      }
     }
     return _addEventListener.apply(this, args);
   };
