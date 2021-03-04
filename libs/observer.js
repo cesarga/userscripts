4fef6df6a9aa4a8708d050c0e16707ff7b5b21eb
César <cesarga@d3riva.com>
2021-03-04 11:47:24 +0100

    Create observer.js

---
+++ libs/observer.js
@@ -0,0 +1,23 @@
+function observe(selectorm, callback) {
+  // Select the node that will be observed for mutations
+  const targetNode = document.querySelector(selector);
+
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: true, childList: true, subtree: true };
+
+  // Callback function to execute when mutations are observed
+  const _callback = function(mutationsList, observer) {
+    // Use traditional 'for loops' for IE 11
+    for(const mutation of mutationsList) {
+      if (mutation.type === 'childList'){
+        callback()
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(_callback);
+
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+}
